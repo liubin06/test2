a0408a6fa26db5c84ead541d0724aaaf356560c7
liubin06 <45156282+liubin06@users.noreply.github.com>
2022-05-28 22:49:39 +0800

    aha~

---
+++ possion_process.py
@@ -0,0 +1,5 @@
+#-*- coding: utf-8 -*-
+'''Solution of Linear Prediction Problems in Stationary Stochastic Processes of Generalized Birth-death Catastrophes Process '''
+import numpy as np
+import math
+
